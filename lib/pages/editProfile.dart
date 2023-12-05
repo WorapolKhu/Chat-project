227a4de3a7a4607e470b7d449ba1ee569983385b
WorapolKhu <93505768+WorapolKhu@users.noreply.github.com>
2023-12-06 00:41:16 +0700

    getCurrentUserUpdate

--- lib/pages/editProfile.dart
+++ lib/pages/editProfile.dart
@@ -1,25 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class EditProfilePage extends StatefulWidget {
-  const EditProfilePage({Key? key}) : super(key: key);
-  static String id = 'EditProfile';
+  static String id = 'edit_profile';
+  const EditProfilePage({super.key});
 
   @override
   _EditProfilePageState createState() => _EditProfilePageState();
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+  final _store = FirebaseFirestore.instance;
+  late User loggedInUser;
 
-  late User _user;
-  late DocumentReference _userDoc;
-  late Map<String, dynamic> userData = {}; // Initialize userData here
-
-  TextEditingController fullNameController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
 
   @override
   void initState() {
@@ -28,45 +25,49 @@ class _EditProfilePageState extends State<EditProfilePage> {
   }
 
   void getCurrentUser() async {
-    _auth.authStateChanges().listen((User? user) {
-      if (user != null) {
-        _user = user;
-        _userDoc = _firestore.collection('users').doc(_user.uid);
+    final user = _auth.currentUser;
+    if (user != null) {
+      // Retrieve user data from Firestore
+      DocumentSnapshot userSnapshot =
+          await _store.collection('users').doc(user.uid).get();
 
-        _userDoc.get().then((DocumentSnapshot documentSnapshot) {
-          if (documentSnapshot.exists) {
-            // Populate data from Firestore
-            userData = documentSnapshot.data() as Map<String, dynamic>;
-            fullNameController.text = userData['displayName'];
-            emailController.text = userData['email'];
-          }
+      if (userSnapshot.exists) {
+        // Extract user information from the Firestore document
+        Map<String, dynamic> userData =
+            userSnapshot.data() as Map<String, dynamic>;
+
+        setState(() {
+          loggedInUser = user;
+          _usernameController.text = userData['name'] ?? 'NA';
+          _emailController.text = userData['email'] ?? 'NA';
         });
-
-        setState(() {});
       }
-    });
+    }
   }
 
-  void saveChanges() async {
+  void updateProfile() async {
     try {
-      // Update user data in Firestore
-      await _userDoc.set({
-        'uid': _user.uid,
-        'displayName': fullNameController.text,
-        'email': emailController.text,
+      await loggedInUser.updateDisplayName(_usernameController.text);
+
+      await _store.collection('users').doc(loggedInUser.uid).update({
+        'name': _usernameController.text,
+        'email': _emailController.text,
       });
 
-      // Update the local user data
-      setState(() {
-        userData['displayName'] = fullNameController.text;
-        userData['email'] = emailController.text;
-      });
-
-      // Print a message or perform any other post-save actions
-      print("Changes saved successfully");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Profile updated successfully!'),
+          duration: Duration(seconds: 2),
+        ),
+      );
     } catch (e) {
-      // Handle errors, e.g., display an error message
-      print("Error saving changes: $e");
+      print(e.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to update profile. Please try again.'),
+          duration: Duration(seconds: 2),
+        ),
+      );
     }
   }
 
@@ -74,124 +75,74 @@ class _EditProfilePageState extends State<EditProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(
-          child: Text(
-            "Profile",
-            style: TextStyle(
-              color: Colors.black,
-            ),
-          ),
-        ),
-        elevation: 1,
+        title: Text('Profile'),
+        centerTitle: true,
       ),
-      body: Container(
-        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
-        child: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
-          child: ListView(
-            children: [
-              Center(
-                child: Stack(
-                  children: [
-                    Container(
-                      width: 130,
-                      height: 130,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 4,
-                          color: Theme.of(context).scaffoldBackgroundColor,
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            spreadRadius: 2,
-                            blurRadius: 10,
-                            color: Colors.black.withOpacity(0.1),
-                            offset: Offset(0, 10),
-                          )
-                        ],
-                        shape: BoxShape.circle,
-                      ),
-                      // Use CircleAvatar with an icon instead of Image.asset
-                      child: CircleAvatar(
-                        backgroundColor:
-                            Colors.transparent, // Set background color
-                        child: Icon(Icons.person_outline,
-                            color: Colors.black12,
-                            size: 100), // Icon and its color
-                      ),
-                    ),
-                  ],
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: 130,
+              height: 130,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 4,
+                  color: Theme.of(context).scaffoldBackgroundColor,
                 ),
-              ),
-              SizedBox(height: 35),
-              Text("Username",
-                  style: TextStyle(fontWeight: FontWeight.w300)),
-              buildTextField("Full Name", fullNameController, false),
-              SizedBox(height: 15),
-              Text("Email",
-                  style: TextStyle(fontWeight: FontWeight.w300)),
-              buildTextField("E-mail", emailController, false),
-              SizedBox(height: 35),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // Button to cancel changes
-                  OutlinedButton(
-                    onPressed: () {
-                      // discard any changes
-                      // Reset text fields to initial values
-                      fullNameController.text = userData['displayName'];
-                      emailController.text = userData['email'];
-                      print("Changes cancelled");
-                    },
-                    child: Text(
-                      "CANCEL",
-                      style: TextStyle(
-                        fontSize: 14,
-                        letterSpacing: 2.2,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  // Button to save changes
-                  ElevatedButton(
-                    onPressed: () {
-                      // save the changes to Firebase
-                      saveChanges();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(horizontal: 50),
-                      elevation: 2,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),
-                    child: Text(
-                      "SAVE",
-                      style: TextStyle(
-                        fontSize: 14,
-                        letterSpacing: 2.2,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
+                boxShadow: [
+                  BoxShadow(
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    color: Colors.black.withOpacity(0.1),
+                    offset: Offset(0, 10),
+                  )
                 ],
+                shape: BoxShape.circle,
               ),
-            ],
-          ),
+              // Use CircleAvatar with an icon instead of Image.asset
+              child: CircleAvatar(
+                backgroundColor: Colors.transparent, // Set background color
+                child: Icon(Icons.person_outline,
+                    color: Colors.black26, size: 100), // Icon and its color
+              ),
+            ),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+              readOnly: true,
+            ),
+            SizedBox(height: 32.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OutlinedButton(
+                  onPressed: () {
+                    // reset the text fields to the current user's data
+                    getCurrentUser();
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Changes discarded.'),
+                        duration: Duration(seconds: 2),
+                      ),
+                    );
+                  },
+                  child: Text('Cancel'),
+                ),
+                ElevatedButton(
+                  onPressed: updateProfile,
+                  child: Text('Save'),
+                ),
+              ],
+            ),
+          ],
         ),
-      ),
-    );
-  }
-
-  Widget buildTextField(String labelText, TextEditingController controller,
-      bool isPasswordTextField) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 35.0),
-      child: TextField(
-        controller: controller,
       ),
     );
   }
